fc8ea4f4d0a77776d760d2dfad96857136ef4362
Gilles Ramstein <gilles187@hotmail.com>
2022-02-13 02:48:05 +0100

    math improvements
    -bracket usage is working
    -basic operations with negative numbers
    -scientific notation for large numbers
    -rounding for small numbers

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const OPERATORS = ["×", "÷", "+", "-"];
+const MAX_DECIMAL_PLACES = 5;
+const MAX_NUMBER_LENGTH = 7;
+SCIENTIFIC_NOTATION = true;
 
-const displayCurrent = document.getElementById("display-current");
-const displayPrevious = document.getElementById("display-previous");
+const displayUpper = document.getElementById("display-previous");
+const displayCenter = document.getElementById("display-expression");
+const displayLower = document.getElementById("display-answer");
 const buttons = Array.from(document.querySelectorAll("button"));
 
 buttons.forEach((button) => button.addEventListener("click", handleClick));
@@ -15,32 +19,64 @@ let previousAnswer = "";
 function handleClick(e) {
     buttonActions[this.dataset.type](this.dataset.value);
 
-    if (stringStartsWithOperator(stringExpression)) {
-        if (previousAnswer === "") {
-            stringExpression = "";
-        } else {
-            stringExpression = previousAnswer + stringExpression;
-        }
+    // insert ANS at beginning if expression is a single operator and ANS exists
+    if (previousAnswer !== "" && stringExpression.match(/^[+×÷-]$/)) {
+        stringExpression = previousAnswer + stringExpression;
     }
-    if (stringEndsWithOperator(stringExpression)) {
-        const n = stringExpression.length;
-        if (stringEndsWithOperator(stringExpression.substring(0, n - 1))) {
-            stringExpression =
-                stringExpression.substring(0, n - 2) +
-                stringExpression.charAt(n - 1);
-        }
-    } else if (stringExpression.endsWith(".")) {
-        // skip evaluation
-    } else {
+
+    // do not allow operator following a bracket or vice versa
+    stringExpression = stringExpression.replace(
+        /\([+×÷]$|[+×÷]\)/,
+        (match) => match[0]
+    );
+
+    // do not allow closing brackets without an opening bracket
+    if (
+        stringExpression.split(")").length - 1 >
+        stringExpression.split("(").length - 1
+    ) {
+        stringExpression = stringExpression.replace(/\)$/, "");
+    }
+
+    // do not allow multiple dots in a number
+    stringExpression = stringExpression.replace(/(?<=\.\d+)\.$/, "");
+    stringExpression = stringExpression.replace(/(?<=\.)\./, "");
+
+    // replace operator if operator follows operator
+    stringExpression = stringExpression.replace(
+        /[+×÷][+×÷]$|--$/,
+        (match) => match[1]
+    );
+
+    // do not allow + × ÷ at the beginning
+    stringExpression = stringExpression.replace(/^[+×÷]$/, "");
+
+    // insert mul operator if number follows a bracket or vice versa
+    stringExpression = stringExpression.replace(
+        /\d\($|\)\d$/,
+        (match) => `${match[0]}×${match[1]}`
+    );
+
+    // continuously evaluate if expression does not end with an operator, dot or opening bracket
+    if (!stringExpression.match(/[+×÷.(-]$/)) {
         console.log("------------------------------------------------");
-        console.log(stringExpression);
-        evaluateStringExpression();
+        stringExpression = stringExpression.replace(
+            /\d-/,
+            (match) => `${match[0]}+-`
+        );
+        stringExpression = stringExpression.replace(
+            /\)-/,
+            (match) => `${match[0]}+-`
+        );
+        console.log("strExp:", stringExpression);
+        answer = evaluateBrackets(closeOpenBrackets(stringExpression));
     }
     updateDisplay();
 }
 
 buttonActions = {
     input,
+    del,
     eval,
     clear,
 };
@@ -49,10 +85,20 @@ function input(value) {
     stringExpression += value;
 }
 
+function del(_) {
+    stringExpression = stringExpression.substring(
+        0,
+        stringExpression.length - 1
+    );
+}
+
 function eval(_) {
-    previousExpression = stringExpression;
+    previousExpression =
+        stringExpression === ""
+            ? previousExpression
+            : closeOpenBrackets(stringExpression);
     stringExpression = "";
-    previousAnswer = answer;
+    previousAnswer = answer === "" ? previousAnswer : answer;
     answer = "";
 }
 
@@ -72,42 +118,123 @@ function stringStartsWithOperator(string) {
     return OPERATORS.some((op) => string.startsWith(op));
 }
 
+function closeOpenBrackets(string) {
+    return string + ")".repeat(countOpenBrackets(string));
+}
+
+function countOpenBrackets(string) {
+    const numOpen = string.split("(").length - 1;
+    const numClose = string.split(")").length - 1;
+    return numOpen - numClose;
+}
+
 function updateDisplay() {
+    let upperLine;
     if (previousAnswer === "") {
-        displayPrevious.textContent = "ans";
+        upperLine = "ans";
     } else {
-        displayPrevious.textContent =
-            "ans=" + previousExpression + "=" + round(previousAnswer, 5);
+        upperLine = "ans=" + previousExpression + "=" + previousAnswer;
     }
-    if (answer === "") {
-        displayCurrent.textContent = stringExpression + "()";
+
+    let centerLine;
+    centerLine = closeOpenBrackets(stringExpression);
+
+    let lowerLine = answer != "" ? "(=" + answer + ")" : "()";
+
+    if (SCIENTIFIC_NOTATION) {
+        displayUpper.textContent = toScientific(upperLine);
+        displayCenter.textContent = toScientific(centerLine);
+        displayLower.textContent = toScientific(lowerLine);
     } else {
-        displayCurrent.textContent =
-            stringExpression + " (=" + round(answer, 5) + ")";
+        displayUpper.textContent = roundAll(upperLine);
+        displayCenter.textContent = roundAll(centerLine);
+        displayLower.textContent = roundAll(lowerLine);
     }
+}
+
+function toScientific(string) {
+    console.log(string);
+    return string
+        .replace(/\+-/g, "-")
+        .split(/(?=[+×\-÷()=\s])|(?<=[+×\-÷()=\s])/g)
+        .map((item) => {
+            if (isNaN(item) || item === "" || item === [] || item === " ") {
+                return item;
+            } else {
+                let rounded = round(item, MAX_DECIMAL_PLACES);
+                if (rounded.length > MAX_NUMBER_LENGTH) {
+                    return String(
+                        Number.parseFloat(item).toExponential(
+                            MAX_NUMBER_LENGTH - 4
+                        )
+                    );
+                } else {
+                    return rounded;
+                }
+            }
+        })
+        .join("");
+}
+
+function roundAll(string) {
+    return string
+        .replace(/\+-/g, "-")
+        .split(/(?=[+×\-÷()=\s])|(?<=[+×\-÷()=\s])/g)
+        .map((item) =>
+            isNaN(item) || item === "" || item === [] || item === " "
+                ? item
+                : round(item, MAX_DECIMAL_PLACES)
+        )
+        .join("");
 }
 
 function round(number, decimals) {
     dec = 10 ** decimals;
-    return Math.round((Number(number) + Number.EPSILON) * dec) / dec;
+    return String(Math.round((Number(number) + Number.EPSILON) * dec) / dec);
 }
 
-function evaluateStringExpression() {
-    let array = splitSequence(stringExpression);
-    while (canBeEvaluated(array)) {
-        const operatorIndex = getNextOperatorIndex(array);
-        console.log("next op idx:", operatorIndex);
-        const value = evalBlock(operatorIndex, array);
-        console.log("eval value:", value);
-        array.splice(operatorIndex - 1, 3, value);
-        console.log("arr in while:", array);
+function evaluateBrackets(strExp) {
+    while (strExp.includes("(")) {
+        console.log("while brackets:", strExp);
+        const lastOpenIndex = strExp.lastIndexOf("(");
+        const closeIndex = strExp.indexOf(")", lastOpenIndex);
+        const bracketExpression = strExp.substring(
+            lastOpenIndex + 1,
+            closeIndex
+        );
+        console.log("bracket Exp:", bracketExpression);
+        console.log(
+            strExp.substring(0, lastOpenIndex),
+            evaluateBasicOperators(bracketExpression),
+            strExp.substring(closeIndex + 1, strExp.length)
+        );
+        strExp = (
+            strExp.substring(0, lastOpenIndex) +
+            evaluateBasicOperators(bracketExpression) +
+            strExp.substring(closeIndex + 1, strExp.length)
+        ).replace(/\+--/g, "+");
+        console.log("late while brackets:", strExp);
     }
-    if (array.length != 1) throw `ValueError: Array is '${array}'`;
-    answer = array[0];
+    return evaluateBasicOperators(strExp);
+}
+
+function evaluateBasicOperators(strExp) {
+    let array = splitSequence(strExp);
+    while (canBeEvaluated(array)) {
+        console.log("while operators:", strExp);
+        const operatorIndex = getNextOperatorIndex(array);
+        // console.log("next op idx:", operatorIndex);
+        const value = evalBlock(operatorIndex, array);
+        // console.log("eval value:", value);
+        array.splice(operatorIndex - 1, 3, value);
+        // console.log("arr in while:", array);
+    }
+    console.log("eval operators:", strExp, "->", array, array.join(""));
+    return array.join("");
 }
 
 function splitSequence(string) {
-    array = string.split(/(?=[+×\-÷])|(?<=[+×\-÷])/g);
+    array = string.split(/(?=[+×÷])|(?<=[+×÷])/g);
     return array;
 }
 
@@ -125,11 +252,12 @@ function getNextOperatorIndex(array) {
         return mulIndex < divIndex ? mulIndex : divIndex;
     // addition and subtraction
     const addIndex = array.findIndex((item) => item === "+");
-    const subIndex = array.findIndex((item) => item === "-");
-    if (subIndex === -1 && addIndex >= 1) return addIndex;
-    if (addIndex === -1 && subIndex >= 1) return subIndex;
-    if (addIndex >= 1 && subIndex >= 1)
-        return addIndex < subIndex ? addIndex : subIndex;
+    if (addIndex >= 1) return addIndex;
+    // const subIndex = array.findIndex((item) => item === "-");
+    // if (subIndex === -1 && addIndex >= 1) return addIndex;
+    // if (addIndex === -1 && subIndex >= 1) return subIndex;
+    // if (addIndex >= 1 && subIndex >= 1)
+    //     return addIndex < subIndex ? addIndex : subIndex;
     return -1;
 }
 
@@ -137,10 +265,10 @@ function evalBlock(index, array) {
     const op = array[index];
     const a = Number(array[index - 1]);
     const b = Number(array[index + 1]);
-    console.log(op, a, b);
+    console.log("block:", a, op, b);
     if (op === "×") return a * b;
     if (op === "÷") return a / b;
     if (op === "+") return a + b;
-    if (op === "-") return a - b;
+    // if (op === "-") return a - b;
     throw `Error: Unknown operator '${op}'!`;
 }
